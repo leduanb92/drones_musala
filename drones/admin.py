0955cfee7d2a56b363d778d6a56d9713e9534f72
Leduan B. Rosell Acosta <leduanb@gmail.com>
2022-10-28 21:18:41 -0400

    Added route to django admin site and registered models in it

--- drones/admin.py
+++ drones/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from drones.models import Drone, Medication
+
 # Register your models here.
+admin.site.register(Drone)
+admin.site.register(Medication)
--- drones_musala/urls.py
+++ drones_musala/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 # from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from drones import views
@@ -25,8 +26,8 @@ router.register(r'drones', views.DroneViewSet)
 router.register(r'medications', views.MedicationViewSet)
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    # path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
